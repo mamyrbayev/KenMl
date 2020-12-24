e6705f318fc6cd73bc898f565a0eeecf452eacbb
Assylkhan Aldanazar <asilkhan-al@mail.ru>
2020-12-24 14:55:32 +0600

    Doughnut is done Top is done

--- src/main/resources/static/tem/js/doughnut-chart.js
+++ src/main/resources/static/tem/js/doughnut-chart.js
@@ -1,8 +1,9 @@
-var valueFor2020 = overallForYears.find(e => e.year == 2020);
-
-var pieNumOverall = document.getElementById('pieNumOverall');
-var pieCompleted = document.getElementById('pieCompleted');
-var pieNumeUnderConstruction = document.getElementById('pieNumeUnderConstruction');
+const valueFor2020 = overallForYears[0];
+let barChartValues = overallForYears;
+barChartValues = barChartValues.splice(0, 1);
+const pieNumOverall = document.getElementById('pieNumOverall');
+const pieCompleted = document.getElementById('pieCompleted');
+const pieNumeUnderConstruction = document.getElementById('pieNumeUnderConstruction');
 
 pieNumOverall.innerHTML = valueFor2020.overall;
 pieCompleted.innerHTML = valueFor2020.completed;
@@ -42,3 +43,17 @@ window.onload = function () {
     var ctx = document.getElementById('myChart').getContext('2d');
     window.myPie = new Chart(ctx, config);
 };
+
+const chartNumberElement = document.getElementById('chartNumbers')
+
+overallForYears.forEach(e => {
+    barChartValues.innerHTML += `
+            <div class="bar-img">
+                <img src="/tem/assets/lines.svg">
+            </div>
+            <div class="chart-number">
+                <p>${e.year}</p>
+                <span>год</span>
+            </div>
+    `;
+});
