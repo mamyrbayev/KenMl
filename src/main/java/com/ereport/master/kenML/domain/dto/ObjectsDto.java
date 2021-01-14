a1a067986e15d4cffb55941f3c0919e56312a3f5
Assylkhan Aldanazar <asilkhan-al@mail.ru>
2021-01-15 02:31:18 +0600

    Top text fixed

--- src/main/java/com/ereport/master/kenML/domain/dto/ObjectsDto.java
+++ src/main/java/com/ereport/master/kenML/domain/dto/ObjectsDto.java
@@ -9,9 +9,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Year;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -28,7 +32,23 @@ public class ObjectsDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date startDate;
 
+    public String getStartDateStr() {
+        if (Objects.isNull(startDate)) {
+            return "";
+        }
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(startDate);
+    }
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date endDate;
+
+    public String getEndDateStr() {
+        if (Objects.isNull(endDate)) {
+            return "";
+        }
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(endDate);
+    }
 
 }
